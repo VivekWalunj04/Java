16dfe556de69f45eb8e6a194a7bc5123a95cbb36
VivekWalunj <vivekwalunj334@gmail.com>
2024-08-03 19:27:45 +0530

    Use of for loop

---
+++ Basic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Basic {
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+        System.out.println("My name is Vivek !");
+    }
+}
+    
+}
---
+++ Star_pattern.java
@@ -0,0 +1,5 @@
+import java.util.*;
+
+public class Star_pattern {
+    
+}
