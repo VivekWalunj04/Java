1d0bd7bea84e526b501b1d32087a3955a8a2f168
VivekWalunj <vivekwalunj334@gmail.com>
2024-08-03 19:36:53 +0530

    Star pattern

--- Star_pattern.java
+++ Star_pattern.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class Star_pattern {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give number of lines ");
+        int NoOfLine = sc.nextInt();
+        for(int Line=1;Line<=NoOfLine;Line++){
+            for(int star=1;star<=Line;star++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
 }
