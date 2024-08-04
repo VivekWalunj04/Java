e4fe7d33efdcd4b698eb9518f52dfa1bd401a9cb
VivekWalunj <vivekwalunj334@gmail.com>
2024-08-04 19:07:33 +0530

    Largest and smallest number is array

---
+++ largestandsmallest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class largestandsmallest {
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;//-infinity
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest number in array is :"+smallest);
+        return largest;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enetr size of array:");
+        int n = sc.nextInt();
+        int numbers[] = new int[n];
+        System.out.println("Enter number in array:");
+        for(int i=0;i<n;i++){
+            numbers[i] =  sc.nextInt();
+        }
+        System.out.println("Largest number in arrat is :"+getLargest(numbers));
+
+    }
+    
+}
